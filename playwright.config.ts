7ed5f6d46677c76cc6e56c65abad68e2fef56075
Maxim Rozhkov <maks.rozhkov.2004@gmail.com>
2025-04-22 23:55:27 +0500

    fix: ci/cd

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@playwright/test";
 import { baseUrl } from "./tests/data/testData";
 
 export default defineConfig({
-    testDir: "./tests/api",
+    testDir: "./src/tests/api",
     fullyParallel: true,
     use: {
         baseURL: baseUrl,
